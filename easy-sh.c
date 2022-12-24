e2cf71ebacf19329aef18767d694fe8ff1db29e5
erfanara <m.erfann.ara@gmail.com>
2022-12-24 23:20:45 +0330

    prettify

--- easy-sh.c
+++ easy-sh.c
@@ -1,4 +1,8 @@
+#include "./colors.h"
 #include <asm-generic/errno-base.h>
+#include <errno.h>
+#include <linux/limits.h>
+#include <signal.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,17 +10,14 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <errno.h>
-#include <linux/limits.h>
-#include <signal.h>
-#include "./colors.h"
-#include <readline/readline.h>
 #include <readline/history.h>
+#include <readline/readline.h>
 
 #define ARGS_LIMIT 5000
 
-void fcprintf(FILE *restrict stream, char* str, char* color, char* after_color){
-        fprintf(stream, "%s%s%s", color, str, after_color);
+void fcprintf(FILE *restrict stream, char *str, char *color,
+              char *after_color) {
+  fprintf(stream, "%s%s%s", color, str, after_color);
 }
 
 pid_t pid = -1;
@@ -33,30 +34,33 @@ void run_command(char *const file, char *const *argv) {
     pid = wait(&status);
   }
 
-  if(status){
+  if (status) {
     switch (errno) {
-      default:
-        if(!pid)
-          fprintf(stderr, "%serrno %i occurred!\n%s" , RED, errno, RESET);
-        break;
-      case ENOENT:
-        if(!pid)
-          fcprintf(stderr, "Command not found!\n", RED, RESET);
-        break;
-      case EACCES:
-        if(!pid)
-          fcprintf(stderr, "Permission denied!\n", RED, RESET);
-        break;
-      case ELOOP:
-        if(!pid)
-          fcprintf(stderr, "A loop exists in symbolic links (Maximum recursion limit exceeded)!\n", RED, RESET);
-        break;
-      case ENAMETOOLONG:
-        if(!pid)
-          fcprintf(stderr, "Path too long!\n", RED, RESET);
-        break;
+    default:
+      if (!pid)
+        fprintf(stderr, "%serrno %i occurred!\n%s", RED, errno, RESET);
+      break;
+    case ENOENT:
+      if (!pid)
+        fcprintf(stderr, "Command not found!\n", RED, RESET);
+      break;
+    case EACCES:
+      if (!pid)
+        fcprintf(stderr, "Permission denied!\n", RED, RESET);
+      break;
+    case ELOOP:
+      if (!pid)
+        fcprintf(stderr,
+                 "A loop exists in symbolic links (Maximum recursion limit "
+                 "exceeded)!\n",
+                 RED, RESET);
+      break;
+    case ENAMETOOLONG:
+      if (!pid)
+        fcprintf(stderr, "Path too long!\n", RED, RESET);
+      break;
     }
-    if(!pid)
+    if (!pid)
       exit(-1);
   }
 }
@@ -72,42 +76,44 @@ void split_to_argv(char *str, char *argv[]) {
   argv[i] = NULL;
 }
 
-
 // Built-ins
-void cd(char* path){
+void cd(char *path) {
   int status = chdir(path);
-  if(status){
+  if (status) {
     switch (errno) {
-      default:
-        break;
-      case EACCES:
-        fcprintf(stderr, "Permission denied!\n", RED, RESET);
-        break;
-      case ELOOP:
-        fcprintf(stderr, "A loop exists in symbolic links (Maximum recursion limit exceeded)!\n", RED, RESET);
-        break;
-      case ENAMETOOLONG:
-        fcprintf(stderr, "Path too long!\n", RED, RESET);
-        break;
-      case ENOENT:
-        fcprintf(stderr, "Wrong path!\n", RED, RESET);
-        break;
-      case ENOTDIR:
-        fcprintf(stderr, "Not a directory!\n", RED, RESET);
-        break;
+    default:
+      break;
+    case EACCES:
+      fcprintf(stderr, "Permission denied!\n", RED, RESET);
+      break;
+    case ELOOP:
+      fcprintf(stderr,
+               "A loop exists in symbolic links (Maximum recursion limit "
+               "exceeded)!\n",
+               RED, RESET);
+      break;
+    case ENAMETOOLONG:
+      fcprintf(stderr, "Path too long!\n", RED, RESET);
+      break;
+    case ENOENT:
+      fcprintf(stderr, "Wrong path!\n", RED, RESET);
+      break;
+    case ENOTDIR:
+      fcprintf(stderr, "Not a directory!\n", RED, RESET);
+      break;
     }
   }
 }
 
-void sigint_handler(int i){
-  rl_replace_line("",0);
+void sigint_handler(int i) {
+  rl_replace_line("", 0);
   rl_redisplay();
-  rl_done=1;
+  rl_done = 1;
 }
 int dummy_event(void) { return 0; }
 
 int main(int argc, char *argv[]) {
-  rl_event_hook=dummy_event;
+  rl_event_hook = dummy_event;
   signal(SIGINT, sigint_handler);
 
   char *cmd_argv[ARGS_LIMIT];
@@ -118,29 +124,30 @@ int main(int argc, char *argv[]) {
 
   rl_bind_key('\t', rl_complete);
 
-  char hist_path[PATH_MAX+6];
-  sprintf(hist_path, "%s/.hist",cwd);
+  char hist_path[PATH_MAX + 6];
+  sprintf(hist_path, "%s/.hist", cwd);
 
   read_history(hist_path);
   using_history();
 
-  char prompt[PATH_MAX+11]; 
-  sprintf(prompt, "%s%s%s$ ",GRN ,cwd, RESET);
+  char prompt[PATH_MAX + 11];
+  sprintf(prompt, "%s%s%s$ ", GRN, cwd, RESET);
 
   while (1) {
-    char* input = readline(prompt);
+    char *input = readline(prompt);
     if (input != NULL && input[0] != '\0') {
-        add_history(input);
-        write_history(hist_path);
+      add_history(input);
+      write_history(hist_path);
 
-        split_to_argv(input, cmd_argv);
-        if(!strcmp(cmd_argv[0],"exit")) break;
-        else if(!strcmp(cmd_argv[0],"cd")){
-          cd(cmd_argv[1]);
-          getcwd(cwd, PATH_MAX);
-        }else{
-          run_command(cmd_argv[0], cmd_argv);
-        }
+      split_to_argv(input, cmd_argv);
+      if (!strcmp(cmd_argv[0], "exit"))
+        break;
+      else if (!strcmp(cmd_argv[0], "cd")) {
+        cd(cmd_argv[1]);
+        getcwd(cwd, PATH_MAX);
+      } else {
+        run_command(cmd_argv[0], cmd_argv);
+      }
     }
     free(input);
   }
